342267eba8ffc4036fce23eddcf75fcf5d1139b8
sanju27121985 <sanju.cloud85@gmail.com>
2025-07-24 10:17:32 +0530

    Initial Comiit Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more ");
+﻿void FirstMethod()
+{ 
+}
+void SecondMethod()
+{
+}
+void ThirdMethod()
+{
+
+}
